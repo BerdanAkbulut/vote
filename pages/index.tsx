7f85662c4ec82da257e2a47ba659fd8399593239
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-06-15 18:11:47 +0300

    configuring prisma and planetscale

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { prisma } from '../db/client';
 
-
-import type { NextPage } from 'next'
-const Home: NextPage = () => {
+import type { NextPage } from 'next';
+const Home: NextPage = (props:any) => {
   return (
-    <div >
-     <h1 className='font-bold'>bed</h1>
+    <div>
+      <h1 className="font-bold">bed</h1>
+      <code>
+        {props.questions}
+      </code>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export const getServerSideProps = async () => {
+  const questions = await prisma.question.findMany();
+
+  return {
+    props: {
+      questions: JSON.stringify(questions),
+    },
+  };
+};
