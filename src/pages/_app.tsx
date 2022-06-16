6a436b10559717ddf1919c8f4182e4ac7efc0bca
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-06-16 15:52:21 +0300

    deleting url content in apptsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,14 +13,12 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = '';
 
     return {
       url,
       transformer: superjson,
-      
+
       /**
        * @link https://react-query.tanstack.com/reference/QueryClient
        */
