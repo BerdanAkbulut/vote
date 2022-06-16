4a8effe9b344410b41b10bcdce218ddd576496f6
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-06-16 04:21:15 +0300

    adding trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { prisma } from '../db/client';
-
+import { prisma } from '../prisma/db/client';
+import { trpc } from '../utils/trpc';
 import type { NextPage } from 'next';
 const Home: NextPage = (props:any) => {
+  const {data,isLoading} = trpc.useQuery(['getAllQuestions']);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1 className="font-bold">bed</h1>
       <code>
         {props.questions}
+        <p>{data[0]?.question}</p>
       </code>
     </div>
   );
@@ -16,12 +23,4 @@ const Home: NextPage = (props:any) => {
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const questions = await prisma.question.findMany();
 
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
-};
