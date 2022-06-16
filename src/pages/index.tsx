5c4618acb3170ff75d18af93d3d7e357bd8bd6bd
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-06-16 16:34:53 +0300

    just path in app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,11 @@ import Image from 'next/image';
 import { prisma } from '../../db/client';
 import { trpc } from '../utils/trpc';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-const Home: NextPage = (props: any) => {
-  const [q1, setQ1] = useState<any>();
-  useEffect(() => {
-    setQ1(trpc.useQuery(['getAllQuestions']));
-  }, []);
+const Home: NextPage = (props:any) => {
+  const {data,isLoading} = trpc.useQuery(['getAllQuestions']);
+  
 
-  if (!q1.data) {
+  if (isLoading) {
     return <div>Loading...</div>;
   }
 
@@ -19,10 +16,12 @@ const Home: NextPage = (props: any) => {
       <h1 className="font-bold">bed</h1>
       <code>
         {props.questions}
-        <p>{q1.data[0].question}</p>
+        <p>{data[0]?.question}</p>
       </code>
     </div>
   );
 };
 
 export default Home;
+
+
